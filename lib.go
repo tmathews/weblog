6dc96331cff8482fbc99b37d2363c5c3fd06e652
Thomas Mathews <thomas@monstercat.com>
2019-01-16 23:03:29 -0800

    Added tags, files, & pages

---
+++ lib.go
@@ -0,0 +1,585 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"html/template"
+	"math"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/dyatlov/go-htmlinfo/htmlinfo"
+	"github.com/gofrs/uuid"
+	"github.com/gosimple/slug"
+)
+
+type Identifier string
+
+type PostType int
+
+const (
+	TypeDefault PostType = 0
+	TypeRepost  PostType = 1
+	TypeHeart   PostType = 2
+	TypeAll     PostType = 3
+)
+
+var (
+	ErrURIUsed         = errors.New("URI in use")
+	ErrContentNotFound = errors.New("content not found")
+	ErrInvalidID       = errors.New("invalid id")
+)
+
+type ContentPiece struct {
+	Body                 string
+	Snippet              string
+	DateCreated          time.Time
+	Date                 time.Time
+	ID                   Identifier
+	ResponseToURL        string
+	Title                string
+	Type                 PostType
+	URI                  string
+	ResponseToURLPreview *URLPreview
+	Tags                 []string
+}
+
+func (c *ContentPiece) HTML() template.HTML {
+	return template.HTML(c.Body)
+}
+
+func (c *ContentPiece) DateString() string {
+	return c.Date.Format("January 2006 2 at 03:04AM")
+}
+
+func (c *ContentPiece) DateInputString() string {
+	return c.Date.Format("2006-01-02")
+}
+
+func (c *ContentPiece) TagString() string {
+	return strings.Join(c.Tags, ", ")
+}
+
+func (c *ContentPiece) TimeInputString() string {
+	return c.Date.Format("15:04")
+}
+
+type URLPreview struct {
+	OembedHTML   template.HTML
+	Snippet      string
+	ThumbnailURL string
+	Title        string
+	URL          string
+	DateCrawled  time.Time
+}
+
+type PageInfo struct {
+	Current   int
+	Previous  int
+	Next      int
+	Total     int
+	ItemTotal int
+	ItemCount int
+	PostType  PostType
+	Tag       string
+}
+
+func (p *PageInfo) HasPrevious() bool {
+	return p.Current > 1
+}
+
+func (p *PageInfo) HasNext() bool {
+	return p.Current < p.Total
+}
+
+func (p *PageInfo) CalculateTotal() {
+	p.Total = int(math.Ceil(float64(p.ItemCount) / float64(p.ItemTotal)))
+	p.Previous = p.Current - 1
+	p.Next = p.Current + 1
+}
+
+func CreateSample(tx *sql.Tx) error {
+	err := CreateContent(tx, &ContentPiece{
+		Title:   "Sample Post",
+		Body:    `<p>I am sample</p>`,
+		Snippet: "I am sample.",
+		Date:    time.Now(),
+		URI:     "sample",
+		Tags:    []string{"sample"},
+	})
+	if err == ErrURIUsed {
+		return nil
+	}
+	return err
+}
+
+func GetContents(db *sql.DB, page *PageInfo) ([]*ContentPiece, error) {
+	args := []interface{}{time.Now()}
+	sql := `SELECT COUNT(id) AS count FROM content WHERE date <= ?`
+	if page.PostType != TypeAll {
+		sql += ` AND type = ?`
+		args = append(args, page.PostType)
+	}
+	stmt, err := db.Prepare(sql)
+	if err != nil {
+		return nil, err
+	}
+	var count int
+	if err := stmt.QueryRow(args...).Scan(&count); err != nil {
+		return nil, err
+	}
+	if count >= 50 {
+		page.ItemCount = 50
+	} else {
+		page.ItemCount = count
+	}
+	page.ItemTotal = count
+	page.CalculateTotal()
+
+	sql = `
+SELECT
+	t1.id,
+	t1.title,
+	t1.body,
+	t1.snippet,
+	t1.uri,
+	t1.date,
+	t1.date_created,
+	t1.type,
+	t1.response_to,
+	IFNULL(t2.url, ""),
+	IFNULL(t2.title, ""),
+	IFNULL(t2.snippet, ""),
+	IFNULL(t2.thumbnail_url, ""),
+	IFNULL(t2.oembed_html, ""),
+	(SELECT IFNULL(GROUP_CONCAT(value, ","), "") FROM tag WHERE id = t1.id) AS tags
+FROM
+	content AS t1
+	LEFT JOIN url_preview AS t2 ON (t1.response_to = t2.url)`
+
+	if page.Tag != "" {
+		sql += `INNER JOIN tag AS t3 ON (t1.id = t3.id) `
+	}
+	sql += `
+WHERE
+	date <= ?`
+	
+	args = []interface{}{time.Now()}
+	if page.PostType != TypeAll {
+		sql += ` AND t1.type = ?`
+		args = append(args, page.PostType)
+	}
+	if page.Tag != "" {
+		sql += ` AND t3.value = ?`
+		args = append(args, page.Tag)
+	}
+	sql += `
+ORDER BY
+	date DESC
+LIMIT ?
+OFFSET ?`
+	args = append(args, page.ItemCount, (page.Current-1)*page.ItemCount)
+
+	stmt, err = db.Prepare(sql)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var xs []*ContentPiece
+	rows, err := stmt.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var a ContentPiece
+		var b URLPreview
+		var tags string
+		if err := rows.Scan(&a.ID,
+			&a.Title,
+			&a.Body,
+			&a.Snippet,
+			&a.URI,
+			&a.Date,
+			&a.DateCreated,
+			&a.Type,
+			&a.ResponseToURL,
+			&b.URL,
+			&b.Title,
+			&b.Snippet,
+			//&b.DateCrawled,
+			&b.ThumbnailURL,
+			&b.OembedHTML,
+			&tags); err != nil {
+			return nil, err
+		}
+		if tags != "" {
+			a.Tags = strings.Split(tags, ",")
+		}
+		if a.ResponseToURL != "" {
+			a.ResponseToURLPreview = &b
+		}
+		fmt.Println(a.Title)
+		xs = append(xs, &a)
+	}
+	return xs, nil
+}
+
+func GetContent(tx *sql.Tx, uri string) (*ContentPiece, error) {
+	stmt, err := tx.Prepare(`
+SELECT
+	t1.id,
+	t1.title,
+	t1.body,
+	t1.snippet,
+	t1.uri,
+	t1.date,
+	t1.date_created,
+	t1.type,
+	t1.response_to,
+	IFNULL(t2.url, ""),
+	IFNULL(t2.title, ""),
+	IFNULL(t2.snippet, ""),
+	IFNULL(t2.thumbnail_url, ""),
+	IFNULL(t2.oembed_html, ""),
+	(SELECT IFNULL(GROUP_CONCAT(value, ","), "") FROM tag WHERE id = t1.id) AS tags
+FROM
+	content AS t1
+	LEFT JOIN url_preview AS t2 ON (t1.response_to = t2.url)
+WHERE
+	uri = ?`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var a ContentPiece
+	var b URLPreview
+	var tags string
+	err = stmt.QueryRow(uri).Scan(&a.ID,
+		&a.Title,
+		&a.Body,
+		&a.Snippet,
+		&a.URI,
+		&a.Date,
+		&a.DateCreated,
+		&a.Type,
+		&a.ResponseToURL,
+		&b.URL,
+		&b.Title,
+		&b.Snippet,
+		//&b.DateCrawled,
+		&b.ThumbnailURL,
+		&b.OembedHTML,
+		&tags)
+	if err == sql.ErrNoRows {
+		return nil, ErrContentNotFound
+	} else if err != nil {
+		return nil, err
+	}
+	if tags != "" {
+		a.Tags = strings.Split(tags, ",")
+	}
+	if a.ResponseToURL != "" {
+		a.ResponseToURLPreview = &b
+	}
+	return &a, nil
+}
+
+func CreateContent(tx *sql.Tx, c *ContentPiece) error {
+	ok, err := IsAvailableURI(tx, c.URI)
+	if err != nil {
+		return err
+	}
+	if !ok {
+		return ErrURIUsed
+	}
+
+	id, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
+	c.ID = Identifier(id.String())
+
+	// Fetch url info if it exists
+	if c.ResponseToURL != "" {
+		if _, err := GetURLPreview(tx, c.ResponseToURL); err == sql.ErrNoRows {
+			p, err := ScrapURLPreview(c.ResponseToURL)
+			if err != nil {
+				return err
+			}
+			if err := PutURLPreview(tx, *p); err != nil {
+				return err
+			}
+		}
+	}
+
+	stmt, err := tx.Prepare(`
+INSERT INTO content (
+	title,
+	body,
+	snippet,
+	date,
+	date_created,
+	id,
+	response_to,
+	type,
+	uri
+) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	if _, err := stmt.Exec(c.Title, c.Body, c.Snippet, c.Date, time.Now(), c.ID, c.ResponseToURL, c.Type, c.URI); err != nil {
+		return err
+	}
+
+	for _, tag := range c.Tags {
+		if err := InsertTag(tx, c.ID, tag); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func InsertTag(tx *sql.Tx, id Identifier, tag string) error {
+	stmt, err := tx.Prepare(`INSERT INTO tag (id, value) VALUES (?, ?)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	if _, err := stmt.Exec(id, strings.TrimSpace(tag)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteTags(tx *sql.Tx, id Identifier) error {
+	stmt, err := tx.Prepare(`DELETE FROM tag WHERE id = ?`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(id)
+	return err
+}
+
+func UpdateContent(tx *sql.Tx, c *ContentPiece, rescrape bool) error {
+	if c.ID == "" {
+		return ErrInvalidID
+	}
+	x, err := GetContent(tx, c.URI)
+	if err != nil && err != ErrContentNotFound {
+		return err
+	}
+	if x != nil && x.ID != c.ID {
+		return ErrURIUsed
+	}
+
+	// It changed!
+	if c.ResponseToURL != "" && ((x != nil && c.ResponseToURL != x.ResponseToURL) || rescrape) {
+		if p, err := GetURLPreview(tx, c.ResponseToURL); err == sql.ErrNoRows || rescrape {
+			fmt.Println("Scrapping")
+			p, err := ScrapURLPreview(c.ResponseToURL)
+			if err != nil {
+				return err
+			}
+			if err := PutURLPreview(tx, *p); err != nil {
+				return err
+			}
+			fmt.Println("Scraped it")
+			fmt.Println(p)
+		} else if err != nil {
+			fmt.Println(2)
+			return err
+		} else {
+			fmt.Println("already has preview")
+			fmt.Println(p)
+		}
+	}
+
+	stmt, err := tx.Prepare(`UPDATE content SET
+	title = ?,
+	body = ?,
+	snippet = ?,
+	date = ?,
+	response_to = ?,
+	uri = ?,
+	type = ?
+	WHERE id = ?;`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(c.Title, c.Body, c.Snippet, c.Date, c.ResponseToURL, c.URI, c.Type, c.ID)
+	if err != nil {
+		return err
+	}
+	if count, err := res.RowsAffected(); err != nil {
+		return err
+	} else if count != 1 {
+		return ErrContentNotFound
+	}
+	if err := DeleteTags(tx, c.ID); err != nil {
+		return err
+	}
+	for _, tag := range c.Tags {
+		if err := InsertTag(tx, c.ID, tag); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func DeleteContent(tx *sql.Tx, c *ContentPiece) error {
+	stmt, err := tx.Prepare(`DELETE FROM content WHERE id = ?`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(c.ID)
+	if err != nil {
+		return err
+	}
+	if count, err := res.RowsAffected(); err != nil {
+		return err
+	} else if count == 0 {
+		return ErrContentNotFound
+	}
+	return DeleteTags(tx, c.ID)
+}
+
+func TitleToURI(s string) string {
+	return slug.Make(s)
+}
+
+func IsAvailableURI(tx *sql.Tx, uri string) (bool, error) {
+	stmt, err := tx.Prepare(`SELECT COUNT(uri) FROM content WHERE uri = ?`)
+	if err != nil {
+		return false, err
+	}
+	defer stmt.Close()
+	var count int
+	if err := stmt.QueryRow(uri).Scan(&count); err != nil {
+		return false, err
+	}
+	if count > 0 {
+		return false, nil
+	}
+	return true, nil
+}
+
+func GetURLPreview(tx *sql.Tx, s string) (*URLPreview, error) {
+	var p URLPreview
+	stmt, err := tx.Prepare(`SELECT
+		url,
+		title,
+		snippet,
+		date_crawled,
+		oembed_html,
+		thumbnail_url
+	FROM url_preview WHERE url = ? LIMIT 1`)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(s).Scan(&p.URL, &p.Title, &p.Snippet, &p.DateCrawled, &p.OembedHTML, &p.ThumbnailURL)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+}
+
+func PutURLPreview(tx *sql.Tx, p URLPreview) error {
+	stmt, err := tx.Prepare(`DELETE FROM url_preview WHERE url = ?`)
+	if err != nil {
+		return err
+	}
+	if _, err := stmt.Exec(p.URL); err != nil {
+		return err
+	}
+	stmt.Close()
+	stmt, err = tx.Prepare(`INSERT INTO url_preview (
+		url,
+		title,
+		snippet,
+		date_crawled,
+		oembed_html,
+		thumbnail_url
+	) VALUES (?, ?, ?, ?, ?, ?)`)
+	if err != nil {
+		return err
+	}
+	if _, err := stmt.Exec(p.URL, p.Title, p.Snippet, p.DateCrawled, string(p.OembedHTML), p.ThumbnailURL); err != nil {
+		return err
+	}
+	stmt.Close()
+	return nil
+}
+
+func ScrapURLPreview(s string) (*URLPreview, error) {
+	resp, err := http.Get(s)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	info := htmlinfo.NewHTMLInfo()
+	if err := info.Parse(resp.Body, &s, nil); err != nil {
+		return nil, err
+	}
+
+	og := info.OGInfo
+	p := URLPreview{
+		URL:         s,
+		Title:       info.Title,
+		Snippet:     info.Description,
+		DateCrawled: time.Now(),
+	}
+	if info.OembedInfo != nil {
+		if info.OembedInfo.HTML != "" {
+			p.OembedHTML = template.HTML(info.OembedInfo.HTML)
+		}
+		if info.OembedInfo.ThumbnailURL != "" {
+			p.ThumbnailURL = info.OembedInfo.ThumbnailURL
+		}
+	} else if og != nil && len(og.Images) > 0 {
+		p.ThumbnailURL = og.Images[0].URL
+	}
+	if og != nil {
+		p.URL = og.URL
+		p.Title = og.Title
+		p.Snippet = og.Description
+	}
+	return &p, nil
+}
+
+func PrepareDb(db *sql.DB) error {
+	_, err := db.Exec(`
+	PRAGMA foreign_keys= ON;
+	CREATE TABLE IF NOT EXISTS content (
+		title STRING,
+		body  STRING,
+		snippet STRING,
+		date DATETIME,
+		date_created DATETIME,
+		id STRING PRIMARY KEY,
+		response_to STRING,
+		type STRING,
+		uri STRING
+	);
+	CREATE TABLE IF NOT EXISTS tag (
+		id STRING,
+		value STRING
+	);
+	CREATE TABLE IF NOT EXISTS url_preview (
+		url STRING PRIMARY KEY,
+		title STRING,
+		snippet STRING,
+		date_crawled DATETIME,
+		oembed_html STRING,
+		thumbnail_url STRING
+	);`)
+	return err
+}
---
+++ server.go
@@ -0,0 +1,371 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+type M map[string]interface{}
+
+type ContentPayload struct {
+	ContentPiece
+	DateString      string
+	TimeString      string
+	TransactionType string
+	OldURI          string
+	Password        string
+	Rescrape        string
+	TagString       string
+}
+
+var (
+	ErrNoAuth = errors.New("not authorized")
+)
+
+func GetPage(c *gin.Context) PageInfo {
+	var page PageInfo
+	pageStr := c.Query("page")
+	if pageInt, err := strconv.Atoi(pageStr); err == nil {
+		if pageInt <= 0 {
+			page.Current = 1
+		} else {
+			page.Current = pageInt
+		}
+	} else {
+		page.Current = 1
+	}
+
+	page.PostType = TypeAll
+	if v, ok := c.GetQuery("type"); ok {
+		switch v {
+		case "post":
+			page.PostType = TypeDefault
+		case "repost":
+			page.PostType = TypeRepost
+		case "heart":
+			page.PostType = TypeHeart
+		}
+	}
+
+	page.Tag = c.Query("tag")
+
+	return page
+}
+
+func IsAuthorized(c *gin.Context) bool {
+	s := sessions.Default(c)
+	val := s.Get("authed")
+	authed, ok := val.(bool)
+	return authed && ok
+}
+
+func StartServer(db *sql.DB, port int, templateGlob, assetsDir, password string) {
+
+	//gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	r.MaxMultipartMemory = 128 << 20
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.LoadHTMLGlob(templateGlob)
+
+	store := cookie.NewStore([]byte(password))
+	r.Use(sessions.Sessions("weblog", store))
+
+	r.NoRoute(func(c *gin.Context) {
+		c.HTML(404, "error.html", M{
+			"Error": "Page not found.",
+		})
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		if IsAuthorized(c) {
+			c.Redirect(302, "./")
+			return
+		}
+		c.HTML(200, "login.html", nil)
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		var payload struct {
+			Password string
+		}
+		if err := c.Bind(&payload); err != nil {
+			c.HTML(500, "login.html", M{
+				"Error": "issue reading payload",
+			})
+			return
+		}
+		if payload.Password != password {
+			c.HTML(500, "login.html", M{
+				"Error": "invalid password",
+			})
+			return
+		}
+		s := sessions.Default(c)
+		s.Set("authed", true)
+		s.Save()
+		c.Redirect(302, "./")
+	})
+
+	r.GET("/logout", func(c *gin.Context) {
+		s := sessions.Default(c)
+		s.Clear()
+		s.Save()
+		c.Redirect(302, "./")
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		page := GetPage(c)
+		xs, err := GetContents(db, &page)
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		scope := M{
+			"Items": xs,
+			"Page":  &page,
+		}
+		if IsReqJSON(c) {
+			c.JSON(200, scope)
+			return
+		}
+		scope["Authorized"] = IsAuthorized(c)
+		c.HTML(200, "all.html", scope)
+	})
+
+	r.GET("/new", func(c *gin.Context) {
+		if !IsAuthorized(c) {
+			HandleError(c, ErrNoAuth)
+			return
+		}
+		sample := ContentPiece{
+			Date: time.Now(),
+		}
+		var editor string
+		switch c.Query("type") {
+		case "repost":
+			editor = "repost"
+		case "heart":
+			editor = "heart"
+		default:
+			editor = "editor"
+		}
+		c.HTML(500, editor+".html", &sample)
+	})
+
+	r.GET("/content/:contentUri", func(c *gin.Context) {
+		tx, err := db.Begin()
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		content, err := GetContent(tx, c.Params.ByName("contentUri"))
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		tx.Commit()
+		if _, ok := c.GetQuery("edit"); ok && IsAuthorized(c) {
+			c.HTML(200, "editor.html", content)
+			return
+		}
+		if IsReqJSON(c) {
+			c.JSON(200, content)
+			return
+		}
+		c.HTML(200, "content.html", M{
+			"Authorized": IsAuthorized(c),
+			"Post":       content,
+		})
+	})
+
+	// Create, update, or delete an author's content
+	r.POST("/content", func(c *gin.Context) {
+		if !IsAuthorized(c) {
+			HandleError(c, ErrNoAuth)
+			return
+		}
+
+		var res ContentPayload
+		if err := c.ShouldBind(&res); err != nil {
+			HandleError(c, err)
+			return
+		}
+
+		if res.DateString == "" || res.TimeString == "" {
+			res.Date = time.Now()
+		} else if d, err := time.Parse("2006-01-02 15:04", res.DateString+" "+res.TimeString); err != nil {
+			HandleError(c, err)
+		} else {
+			res.Date = d
+		}
+
+		res.Tags = strings.Split(res.TagString, ",")
+
+		if res.ResponseToURL == "" && (res.Type == TypeHeart || res.Type == TypeRepost) {
+			HandleError(c, errors.New("missing response url"))
+			return
+		}
+
+		if res.URI == "" {
+			if res.Title != "" {
+				res.URI = TitleToURI(res.Title)
+			} else {
+				res.URI = strconv.FormatInt((time.Now().Unix()), 10)
+			}
+		}
+
+		tx, err := db.Begin()
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		loc := "./content/" + res.URI
+		switch res.TransactionType {
+		case "DELETE":
+			err = DeleteContent(tx, &res.ContentPiece)
+			loc = "./"
+			break
+		case "UPDATE":
+			err = UpdateContent(tx, &res.ContentPiece, res.Rescrape == "on")
+			break
+		default:
+			err = CreateContent(tx, &res.ContentPiece)
+			break
+		}
+		if err != nil {
+			tx.Rollback() // Log it?
+			HandleError(c, err)
+			return
+		}
+		if err := tx.Commit(); err != nil {
+			HandleError(c, err)
+			return
+		}
+		if _, ok := c.GetQuery("json"); ok {
+			c.JSON(201, res.ContentPiece)
+			return
+		}
+		c.Redirect(302, loc)
+	})
+
+	// Alias "page/my-page" for assets directory file finding of "assets/my-page.html"
+	r.GET("/page/:filename", func(c *gin.Context) {
+		c.File(filepath.Join(assetsDir, "pages", c.Params.ByName("filename") + ".html"))
+	})
+
+	r.POST("/files", func(c *gin.Context) {
+		dir := c.PostForm("Directory")
+		h, err := c.FormFile("File")
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		src, err := h.Open()
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		defer src.Close()
+		if err := os.MkdirAll(path.Join(assetsDir, dir), 0755); err != nil {
+			HandleError(c, err)
+			return
+		}
+		dst, err := os.OpenFile(path.Join(assetsDir, dir, h.Filename), os.O_RDWR|os.O_TRUNC|os.O_CREATE,0755)
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		defer dst.Close()
+		if _, err := io.Copy(dst, src); err != nil {
+			HandleError(c, err)
+			return
+		}
+		c.Redirect(302, filepath.Join("/files", dir))
+	})
+
+	r.GET("/files/*path", func(c *gin.Context) {
+		p := c.Params.ByName("path")
+		filename := path.Join(assetsDir, p)
+		fi, err := os.Stat(filename)
+		if err != nil {
+			HandleError(c, err)
+			return
+		}
+		if _, ok := c.GetQuery("delete"); ok {
+			if err := os.RemoveAll(filename); err != nil {
+				HandleError(c, err)
+				return
+			}
+			c.HTML(200, "notice.html", map[string]string{
+				"Message": fmt.Sprintf("Deleted file %s", p),
+				"ReturnURL": filepath.Join("/files", filepath.Dir(p)),
+			})
+			return
+		}
+		if fi.IsDir() {
+			var files []FileItem
+			if err := filepath.Walk(filename, func(path string, info os.FileInfo, err error) error {
+				if path == filename {
+					return nil
+				}
+				if err != nil {
+					return nil
+				}
+				var f FileItem
+				f.Path = filepath.Join(p, path[len(filename):])
+				f.Filename = info.Name()
+				f.IsDirectory = info.IsDir()
+				files = append(files, f)
+				return nil
+			}); err != nil {
+				HandleError(c, err)
+				return
+			}
+			c.HTML(200, "files.html", M{
+				"Directory": p,
+				"Files":     files,
+			})
+			return
+		}
+		c.File(filename)
+	})
+
+	r.Run(":" + strconv.Itoa(port))
+}
+
+type FileItem struct {
+	Filename    string
+	Path        string
+	URI         string
+	IsDirectory bool
+}
+
+func HandleError(c *gin.Context, err error) {
+	w := map[string]string{"Error": err.Error()}
+	if _, ok := c.GetQuery("json"); ok {
+		c.JSON(500, w)
+		return
+	}
+	c.HTML(500, "error.html", w)
+}
+
+func IsReqJSON(c *gin.Context) bool {
+	if _, ok := c.GetQuery("json"); ok {
+		return true
+	}
+	return false
+}
